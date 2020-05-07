b7016498ae022bd3bd42884f0fbc61af84881c42
Spooks <indiana-jonas1@hotmail.com>
2020-05-07 15:53:15 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Wizard wizard1 = new Wizard();
+
+            for (int i = 0; i < wizard1.spellList.Count; i++)
+            {
+                Console.WriteLine(wizard1.spellList[i].element);
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
