31b9d1f8dff1db2004a6f7fd9ddcd439cb0ac040
Spooks <indiana-jonas1@hotmail.com>
2020-05-07 15:58:17 +0200

    Add files via upload

---
+++ ConsoleApp1/Physical.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Physical : Klass
+    {
+
+        public List<string> skillList = new List<string>(); //not supposed to be string, make a class called Skill
+
+    }
+}
---
+++ ConsoleApp1/Wizard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Wizard : Magical
+    {
+
+        Spell fireball = new Fireball();
+        Spell thunder = new Thunder();
+
+        public Wizard()
+        {
+            hp = 120;
+            sp = 200;
+
+            damage = 10;
+            defense = 5;
+            agility = 15;
+
+
+
+            spellList.Add(fireball);
+            spellList.Add(thunder);
+
+        }
+
+    }
+}
